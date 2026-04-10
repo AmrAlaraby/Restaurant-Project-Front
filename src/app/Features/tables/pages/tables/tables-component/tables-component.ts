196eeda2314e41f7d67fa832988d141401953c4e
areejkammoush10 <areejkammoush@gmail.com>
2026-04-10 23:13:02 +0200

    feat(tables): implement add table modal and filtering functionality

--- src/app/Features/tables/pages/tables/tables-component/tables-component.ts
+++ src/app/Features/tables/pages/tables/tables-component/tables-component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableInterface } from '../../../../../Core/Models/TableModules/table-interface';
 import { TableService } from '../../../../../Core/Services/Table-Service/table-service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './tables-component.html',
   styleUrls: ['./tables-component.scss']
 })
@@ -59,4 +60,45 @@ export class TablesComponent implements OnInit {
 get availableCount(): number {
   return this.tables.filter(t => !t.isOccupied).length;
 }
+showAddModal = false;
+newTable = {
+  branchId: 1,
+  tableNumber: '',
+  capacity: 1
+};
+
+openAddModal(): void {
+  console.log('clicked');
+  this.showAddModal = true;
+
+  setTimeout(() => {
+    console.log('modal state:', this.showAddModal);
+  }, 1000);
+}
+
+
+closeAddModal(): void {
+  this.showAddModal = false;
+  this.newTable = { branchId: 1, tableNumber: '', capacity: 1 };
+}
+
+submitAddTable(): void {
+  this.tableService.createTable(this.newTable).subscribe({
+    next: () => {
+      this.loadTables();
+      this.closeAddModal();
+    },
+    error: () => this.errorMessage = 'Failed to create table'
+  });
+  }
+  selectedStatus: boolean | null = null;
+
+filterByStatus(status: boolean | null): void {
+  this.selectedStatus = status;
+  this.tableService.getAllTables(undefined, status ?? undefined).subscribe({
+    next: (res) => this.tables = res,
+    error: () => this.errorMessage = 'Failed to filter tables'
+  });
+}
+  
 }
