4e0f8ee92a9f0ed7dd7c8bcf108c4f2442a7952d
areejkammoush10 <areejkammoush@gmail.com>
2026-04-10 10:18:25 +0200

    feat(tables): implement table management module with CRUD operations and UI

---
+++ src/app/Core/Models/TableModules/table-interface.ts
@@ -0,0 +1,9 @@
+export interface TableInterface {
+
+  id: number;
+  tableNumber: string;
+  capacity: number;
+  isOccupied: boolean;
+  branchId: number;
+}
+
---
+++ src/app/Features/tables/pages/tables/tables-component/tables-component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableInterface } from '../../../../../Core/Models/TableModules/table-interface';
+import { TableService } from '../../../../../Core/Services/Table-Service/table-service';
+
+
+@Component({
+  selector: 'app-tables',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tables-component.html',
+  styleUrls: ['./tables-component.scss']
+})
+export class TablesComponent implements OnInit {
+  tables: TableInterface[] = [];
+  isLoading = false;
+  errorMessage = '';
+
+  constructor(private tableService: TableService) {}
+
+  ngOnInit(): void {
+    this.loadTables();
+  }
+
+  loadTables(): void {
+    this.isLoading = true;
+    this.tableService.getAllTables().subscribe({
+      next: (res) => {
+        this.tables = res;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorMessage = 'Failed to load tables';
+        this.isLoading = false;
+        console.log(err)
+
+
+      }
+    });
+  }
+
+  updateStatus(id: number, isOccupied: boolean): void {
+    this.tableService.updateTableStatus(id, { isOccupied }).subscribe({
+      next: () => this.loadTables(),
+      error: (err) => this.errorMessage = 'Failed to update status'
+    });
+  }
+
+  deleteTable(id: number): void {
+    this.tableService.deleteTable(id).subscribe({
+      next: () => this.loadTables(),
+      error: (err) => this.errorMessage = 'Failed to delete table'
+    });
+  }
+  get occupiedCount(): number {
+  return this.tables.filter(t => t.isOccupied).length;
+}
+
+get availableCount(): number {
+  return this.tables.filter(t => !t.isOccupied).length;
+}
+}
