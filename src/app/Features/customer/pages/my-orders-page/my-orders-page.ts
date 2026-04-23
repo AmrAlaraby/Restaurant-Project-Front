c4ab5d42d999fa88461e30c8c8b5feb0ac053a47
areejkammoush10 <areejkammoush@gmail.com>
2026-04-23 20:29:29 +0200

    feat: implement Active Order Card component with styles, template, and tests

--- src/app/Features/customer/pages/my-orders-page/my-orders-page.ts
+++ src/app/Features/customer/pages/my-orders-page/my-orders-page.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { OrdersService } from '../../../../Core/Services/Orders-Service/orders-service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-orders-page',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './my-orders-page.html',
   styleUrl: './my-orders-page.scss',
 })
 export class MyOrdersPage {
-
 }
