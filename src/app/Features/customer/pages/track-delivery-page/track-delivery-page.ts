bab0b30ff0089af9b5ad0c7998c2aad72b4954a6
areejkammoush10 <areejkammoush@gmail.com>
2026-04-23 23:43:34 +0200

    feat: add track delivery page with HTML, SCSS, and unit tests

---
+++ src/app/Features/customer/pages/track-delivery-page/track-delivery-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-delivery-page',
+  imports: [],
+  templateUrl: './track-delivery-page.html',
+  styleUrl: './track-delivery-page.scss',
+})
+export class TrackDeliveryPage {
+
+}
