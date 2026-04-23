e466bccce9f4f6770f57a95a5fe43ce95a0ce84a
areejkammoush10 <areejkammoush@gmail.com>
2026-04-23 20:49:01 +0200

    feat: add OrdersEmptyState component with HTML, SCSS, and unit tests

---
+++ src/app/Features/customer/components/my-orders/orders-empty-state/orders-empty-state.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-empty-state',
+  imports: [],
+  templateUrl: './orders-empty-state.html',
+  styleUrl: './orders-empty-state.scss',
+})
+export class OrdersEmptyState {
+
+}
