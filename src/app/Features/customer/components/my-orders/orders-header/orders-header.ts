cbbfe575502e2323956707ebbddb54033980cc3e
areejkammoush10 <areejkammoush@gmail.com>
2026-04-23 20:42:53 +0200

    feat: add OrdersHeader component with HTML, SCSS, and unit tests

---
+++ src/app/Features/customer/components/my-orders/orders-header/orders-header.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-header',
+  templateUrl: './orders-header.html',
+  styleUrl: './orders-header.scss',
+})
+export class OrdersHeader {}
