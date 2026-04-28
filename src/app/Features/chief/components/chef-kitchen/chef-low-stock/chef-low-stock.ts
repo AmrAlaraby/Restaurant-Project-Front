096260ca5e743d86059180fa7c323aa1ffaf8565
AmrAlaraby <amr2amr3@gmail.com>
2026-04-29 02:23:25 +0300

    feat(localization): add Arabic and English translations for low stock notifications and update chef low stock component

--- src/app/Features/chief/components/chef-kitchen/chef-low-stock/chef-low-stock.ts
+++ src/app/Features/chief/components/chef-kitchen/chef-low-stock/chef-low-stock.ts
@@ -2,11 +2,14 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BranchStockInterface } from '../../../../../Core/Models/BranchStockModels/BranchStockInterface';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Subject, takeUntil } from 'rxjs';
+import { LocalizationService } from '../../../../../Core/Services/Localization-Service/localization-service';
 
 @Component({
   selector: 'app-chef-low-stock',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslatePipe],
   templateUrl: './chef-low-stock.html',
   styleUrl: './chef-low-stock.scss'
 })
@@ -15,6 +18,27 @@ export class ChefLowStockComponent {
   @Input() stocks: BranchStockInterface[] = [];
 
   requestSent = false;
+
+  constructor(
+          private localizationService: LocalizationService,
+        ) {}
+      ngOnInit(): void {
+        this.getCurrentLanguage();
+      }
+      CurrentLanguage: string = 'en';
+      
+        private destroy$ = new Subject<void>();
+        getCurrentLanguage(): void {
+          this.CurrentLanguage = this.localizationService.getCurrentLang();
+          this.localizationService.currentLang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
+            this.CurrentLanguage = lang;
+          });
+        }
+      
+        ngOnDestroy(): void {
+          this.destroy$.next();
+          this.destroy$.complete();
+        }
 
   getBarPercent(item: BranchStockInterface): number {
     if (!item.lowThreshold || item.lowThreshold === 0) return 0;
@@ -25,5 +49,11 @@ export class ChefLowStockComponent {
   requestRestock(): void {
     this.requestSent = true;
     setTimeout(() => (this.requestSent = false), 3000);
+  }
+  getIngredientName(item: any): string {
+    if (this.CurrentLanguage === 'ar') {
+      return item.ingredientArabicName || item.ingredientName;
+    }
+    return item.ingredientName;
   }
 }
