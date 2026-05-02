773c0ee30f6f874dd36b9e30146ed2f305c8c8a1
AmrAlaraby <amr2amr3@gmail.com>
2026-05-02 16:30:12 +0300

    feat(localization): add localization support for user management section including details, filters, and lists

--- src/app/Features/admin/components/User/users-filters/users-filters.ts
+++ src/app/Features/admin/components/User/users-filters/users-filters.ts
@@ -2,12 +2,15 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BranchDto } from '../../../../../Core/Models/BranchModels/Branch-dto';
 import { UsersService } from '../../../../../Core/Services/User-Service/users-service';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Subject, takeUntil } from 'rxjs';
+import { LocalizationService } from '../../../../../Core/Services/Localization-Service/localization-service';
 
 
 @Component({
   selector: 'users-filters',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,TranslatePipe],
   templateUrl: './users-filters.html',
   styleUrls: ['./users-filters.scss'],
 })
@@ -23,7 +26,7 @@ export class UsersFilters implements OnInit {
   selectedBranchId: number | null = null;
   isActive = true;
 
-  constructor(private usersService: UsersService) {}
+  constructor(private localizationService: LocalizationService,private usersService: UsersService) {}
 
   ngOnInit() {
     this.usersService.getRoles().subscribe({
@@ -35,7 +38,23 @@ export class UsersFilters implements OnInit {
       next: (branches) => (this.branches = branches),
       error: (err) => console.error('Failed to load branches', err),
     });
+    this.getCurrentLanguage();
   }
+
+  CurrentLanguage: string = 'en';
+    
+      private destroy$ = new Subject<void>();
+      getCurrentLanguage(): void {
+        this.CurrentLanguage = this.localizationService.getCurrentLang();
+        this.localizationService.currentLang$.pipe(takeUntil(this.destroy$)).subscribe((lang) => {
+          this.CurrentLanguage = lang;
+        });
+      }
+    
+      ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+      }
 
   selectRole(role: string) {
     this.selectedRole = role;
@@ -50,5 +69,11 @@ export class UsersFilters implements OnInit {
   selectStatus(status: boolean) {
     this.isActive = status;
     this.statusChanged.emit(status);
+  }
+  getItemName(item: any): string {
+    if (this.CurrentLanguage === 'ar') {
+      return item.arabicName || item.name;
+    }
+    return item.name;
   }
 }
